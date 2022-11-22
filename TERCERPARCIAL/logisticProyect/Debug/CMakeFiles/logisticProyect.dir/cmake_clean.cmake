3265951cd43d84f492a32cf9d7494f21731b38e7
Daniel Olarte <80644486+DanielOlarte-GitHub@users.noreply.github.com>
2022-11-21 19:49:14 -0500

    Add files via upload

---
+++ TERCERPARCIAL/logisticProyect/Debug/CMakeFiles/logisticProyect.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/logisticProyect.dir/ClaseLogistic/regressionlogistic.cpp.o"
+  "CMakeFiles/logisticProyect.dir/ClaseLogistic/regressionlogistic.cpp.o.d"
+  "CMakeFiles/logisticProyect.dir/Extraccion/extraction.cpp.o"
+  "CMakeFiles/logisticProyect.dir/Extraccion/extraction.cpp.o.d"
+  "CMakeFiles/logisticProyect.dir/main.cpp.o"
+  "CMakeFiles/logisticProyect.dir/main.cpp.o.d"
+  "logisticProyect"
+  "logisticProyect.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/logisticProyect.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
